60363b9c82a904540d70281c8c08345a97d12f7b
Sergey <webdev@select-pixel.com>
2012-12-08 19:31:54 +0200

    +accounts list demo

---
+++ StorageMonster.Web/Controllers/StorageAccountsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorageMonster.Web.Controllers
+{
+    public class StorageAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class AccountsModel
+    {
+        public IEnumerable<StorageAccount> Accounts { get; set; }
+    }
+    public class StorageAccountsController : Controller
+    {
+        public ActionResult List()
+        {
+            var accounts = new List<StorageAccount>
+                {
+                    new StorageAccount {Id =1, Name = "Name1"},
+                    new StorageAccount {Id =2, Name = "Name2"},
+                };
+            return Json(new AccountsModel {Accounts = accounts}, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ StorageMonster.Web/Scripts/monster-script.js
@@ -0,0 +1,31 @@
+﻿var AccountsList = Backbone.Model.extend({
+    initialize: function (url) {
+        var uri = (!!url)?url.url:this.get('url');
+        try {
+            this.fetchData(uri);
+        }catch(e){
+            console.error(e);
+        }
+        console.log(this.get('accounts'));
+    },
+    defaults: {
+        url: '/StorageAccounts/list/default' 
+    },
+    fetchData: function (uri) {
+        var accounts = this;
+        $.ajax({
+            url: uri,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                if (!accounts.get('accounts')) {
+                    accounts.set({ 'accounts': data });
+                }
+            },
+            error: function () {
+                throw "error fetch accounts";
+            }
+      });
+    }
+});
+
