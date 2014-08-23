86166b7253371daa96f5cc4b82608a4a9f53a76b
cloudguy <cloudguy@yandex.ru>
2014-08-24 02:01:57 +0300

    +routes

--- CloudBin/CloudBin.Web.UI/Services/Configuration/RouteConfiguration.cs
+++ CloudBin/CloudBin.Web.UI/Services/Configuration/RouteConfiguration.cs
@@ -11,10 +11,24 @@ namespace CloudBin.Web.UI.Services.Configuration
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRouteLowercase(
+                "SignIn", // Route name
+                "SignIn", // URL with parameters
+                new { controller = "Account", action = "SignIn" });
+
+            routes.MapRouteLowercase(
+                "SignOut", // Route name
+                "SignOut", // URL with parameters
+                new { controller = "Account", action = "SignOut" });
+
+            routes.MapRouteLowercase(
+                "Register", // Route name
+                "Register", // URL with parameters
+                new { controller = "Account", action = "Register" });
+
+            routes.MapRouteLowercase(
                 "Default", // Route name
                 "{controller}/{action}", // URL with parameters
-                new {controller = "Main", action = "Index"} // Parameter defaults
-                );
+                new {controller = "Main", action = "Index"});
         }
     }
 }
