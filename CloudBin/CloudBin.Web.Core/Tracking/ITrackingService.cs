ff216d467af877e35870d170db8e5f847653c09e
cloudguy <cloudguy@yandex.ru>
2014-08-24 01:37:58 +0300

    -

--- CloudBin/CloudBin.Web.Core/Tracking/ITrackingService.cs
+++ CloudBin/CloudBin.Web.Core/Tracking/ITrackingService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CloudBin.Web.Core.Tracking
+﻿namespace CloudBin.Web.Core.Tracking
 {
     public interface ITrackingService
     {
